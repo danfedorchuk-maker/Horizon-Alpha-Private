e5c30dd92e34d1e511e709ece3bf8032dbf0e4cc
danfedorchuk-maker <dan.fedorchuk@gmail.com>
2026-05-03 22:35:08 -0700

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const apiKey = process.env.GROQ_API_KEY;
   const { word, tradition } = req.body;
 
